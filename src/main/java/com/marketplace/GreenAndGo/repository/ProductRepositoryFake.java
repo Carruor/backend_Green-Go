5104783e626180baa94e52252fce7a33b69497f3
MayuRuru <aruizrull@protonmail.com>
2022-02-08 15:58:34 +0100

    url image

--- src/main/java/com/marketplace/GreenAndGo/repository/ProductRepositoryFake.java
+++ src/main/java/com/marketplace/GreenAndGo/repository/ProductRepositoryFake.java
@@ -16,9 +16,9 @@ public class ProductRepositoryFake implements ProductRepository{
     public List<Product> getAll() {
         Seller seller1 = new Seller(1,"p","p@p.com");
         List <Product> productList = new ArrayList<Product>();
-        Product product1 = new Product(1,"planta", "url", "bonita", seller1, 15 );
-        Product product2 = new Product(2,"plantaz", "url", "bonitx", seller1, 15 );
-        Product product3 = new Product(3,"plantax", "url", "bonitz", seller1, 15 );
+        Product product1 = new Product(1,"planta", "http://localhost:3000/media/planta1.jpg", "bonita", seller1, 15 );
+        Product product2 = new Product(2,"plantaz", "http://localhost:3000/media/planta2.png", "bonitx", seller1, 15 );
+        Product product3 = new Product(3,"plantax", "http://localhost:3000/media/planta3.jpg", "bonitz", seller1, 15 );
         productList.add(product1);
         productList.add(product2);
         productList.add(product3);
